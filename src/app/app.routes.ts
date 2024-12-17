1a607f62567c15147e41323e5d83e9d18ba611a4
hadywafa <97hadywafa@gmail.com>
2024-12-17 21:37:34 +0200

    rename

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from "@angular/router";
-import { MainPageComponent } from "./pages/main-page/main-page.component";
+import { MainPageComponent } from "./components/main-page/main-page.component";
 import { PlayerComponent } from "./components/player/player.component";
 export const routes: Routes = [
   { path: "mainpage", component: MainPageComponent },
